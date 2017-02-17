4fe9c6b486a2fbdd67c03f25190089b36e384139
mhussien86 <m.hussien.android@gmail.com>
2017-02-17 17:34:42 +0200

    Creating empty classes for members list feature

---
+++ app/src/main/java/com/backbase/boardmembers/ui/SplashScreen.java
@@ -0,0 +1,7 @@
+package com.backbase.boardmembers.ui;
+
+/**
+ * Created by mohamed on 17/02/17.
+ */
+public class SplashScreen {
+}
---
+++ app/src/main/java/com/backbase/boardmembers/ui/memberslist/MembersListPresenterImpl.java
@@ -0,0 +1,7 @@
+package com.backbase.boardmembers.ui.memberslist;
+
+/**
+ * Created by mohamed on 17/02/17.
+ */
+public class MembersListPresenterImpl {
+}
